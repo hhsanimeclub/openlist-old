73021db45777335b74484c30ec1e3287c0c06e11
stampylongr <contact@rororobby.ml>
2020-11-06 17:11:25 +0000

    all

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -20,6 +20,7 @@
           </div>
         </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -36,5 +37,67 @@
   overflow: visible;
   padding: 60px 80px;
   width: 100%;
+}
+
+.feature-cards {
+  display: grid;
+  grid-gap: 70px 60px;
+  grid-template-columns: repeat(2, minmax(300px, 400px));
+  justify-content: center;
+  margin: 0 auto;
+  margin-bottom: 90px;
+  margin-top: 85px;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.feature-card {
+  display: grid;
+  grid-template-columns: 80px auto;
+  grid-gap: 40px;
+}
+
+.title {
+  color: #ecf6fe;
+  font-size: 1.8rem;
+  font-weight: 600;
+  margin: 0;
+  margin-bottom: 8px;
+  padding: 0;
+}
+
+.description {
+  color: #acd5f2;
+  font-size: 1.4rem;
+  line-height: 146%;
+}
+
+.heading {
+  color: #f0f3f6;
+  font-size: 3.2rem;
+  font-weight: 700;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  text-align: center;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.heading span {
+  display: none;
+}
+
+.sub-heading {
+  color: #acd5f2;
+  font-size: 2.1rem;
+  line-height: 142%;
+  margin: 0 auto;
+  max-width: 370px;
+  text-align: center;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 </style>
