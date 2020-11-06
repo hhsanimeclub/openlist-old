9d4892302b7c7173f8a3a57159615d16a5e26caa
stampylongr <contact@rororobby.ml>
2020-11-06 16:31:44 +0000

    add stuff

---
+++ src/components/Landing.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="landing">
+    <h1 class="heading">
+      <span>OpenList: </span> Reverse-enginnering AniList one step at a time
+    </h1>
+    <h2 class="sub-heading">
+      Track, share, and discover your favorite anime and manga, all on an
+      open-source website.
+    </h2>
+    <div class="feature-cards">
+      <div class="feature-card">
+        <div class="icon">
+          <img src="../assets/landing/stats.svg" />
+        </div>
+        <div>
+          <h3 class="title">Discover the best shows</h3>
+          <div class="description">
+            What do you like to watch? Who are your favorite voice actors? Watch
+            your habits over time with detailed statistics.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.landing {
+  background: #0a1625;
+  border-radius: 20px;
+  font-family: Overpass, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  margin: 0 auto;
+  margin-bottom: 70px;
+  max-width: 1100px;
+  overflow: visible;
+  padding: 60px 80px;
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="OpenList logo" src="../assets/logo.png" />
+    <Landing />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Landing from "@/components/Landing.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Landing
   }
 };
 </script>
